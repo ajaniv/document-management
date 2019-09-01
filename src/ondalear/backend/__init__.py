f26f3f24e029282de956ca7f9c6aa63975e96028
Amnon Janiv <amnon.janiv@ondalear.com>
2019-09-01 19:32:15 -0400

    add version

--- src/ondalear/backend/__init__.py
+++ src/ondalear/backend/__init__.py
@@ -3,3 +3,11 @@
    :synopsis: ondalear backend package
 
 """
+__title__ = 'OndaLear Document Management Back End'
+__version__ = '0.0.1'
+__author__ = 'Amnon Janiv'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2019 OndaLear LLC'
+
+# Version synonym
+VERSION = __version__
