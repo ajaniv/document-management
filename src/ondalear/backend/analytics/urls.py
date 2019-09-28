69fc0975312942be8ec070ed13e685ecb25ce3d1
Amnon Janiv <amnon.janiv@ondalear.com>
2019-09-27 20:14:07 -0400

    invalid url format

--- src/ondalear/backend/analytics/urls.py
+++ src/ondalear/backend/analytics/urls.py
@@ -3,3 +3,10 @@
    :synopsis: ondalear analytics urls  module.
 
 """
+from django.urls import path
+from ondalear.backend.docmgmt import views
+
+
+urlpatterns = [
+    path('index/', views.index, name='index'),
+]
